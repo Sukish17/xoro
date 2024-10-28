f7b50e838b9ac31469c0caac68c4e6cce2b85e1d
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-28 13:09:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:3000", credentials: true}));
+app.use(cors({origin: "https://xoro-front.vercel.app/login", credentials: true}));
 
 // Connect to MongoDB
 connectDB();
