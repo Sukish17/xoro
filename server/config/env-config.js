fe93490cd2c9fed5081b1fe73210aed489fed3f6
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-29 11:08:56 +0530

    Update env-config.js

--- server/config/env-config.js
+++ server/config/env-config.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const JWT_SECRET = 'jbkjwhelken';
-export const PORT = 3001;
+export const PORT = 'https://rubyxoroserver.vercel.app';
 export const MONGODB_URI = 'mongodb+srv://sukishkohli:Sukish17@cluster0.dqpo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
