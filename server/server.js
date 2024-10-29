502369401cd7b0617cf7bdf90a7caf9b008b5ac8
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-29 10:39:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
+
 import express from "express";
 import projectsRouter from "./routes/project-routes.js";
 import tasksRouter from "./routes/task-routes.js";
 import healthcheckRouter from "./routes/healthcheck-routes.js";
 import authRouter from "./routes/auth-routes.js";
+import { PORT } from "./config/env-config.js";
 import cors from "cors";
 import connectDB from "./db/index.js";
 import cookieParser from 'cookie-parser';
 
-const PORT = 'https://xoro-backend.vercel.app';
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "https://xoro-front.vercel.app", credentials: true}));
+app.use(cors({origin: "http://localhost:3000", credentials: true}));
 
 // Connect to MongoDB
 connectDB();
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`); // Log the HTTP link
+  console.log(`Server is running on http://localhost:${PORT}`); // Log the HTTP link
 });
 
 // Our project routes go here
